0de6f4530446f95e6f6b0d49dab9e67dc0931722
Romualdo Santiago Santiago <rsantiago@inndot.com>
2019-01-03 10:50:32 -0600

    Se agrega el framework de codeigniter v. 3.1.9 para pruebas de watson-marketing

---
+++ application/controllers/Inicio.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Inicio extends CI_Controller {
+	public function index()
+	{
+		//$this->load->library('watsonmarketing');
+		// $this->load->view('inicio/principal');
+		echo "Bienvenido"; 
+	}	
+}
