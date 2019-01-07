8d00ff68b21c62f7f8f290c05498eaa03499784e
Romualdo Santiago Santiago <rsantiago@inndot.com>
2019-01-07 14:20:38 -0600

    Se adapta el codigo para hacer una clase que se divida por metodos (orientado a objetos)

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -2,69 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Inicio extends CI_Controller {
 	public function index()
-	{		
-		// $this->load->view('inicio/principal');
-		echo "Bienvenido"; 
-	}
-	
-	public function conecta(){
-	//	prueba de codigo referencia-> https://developer.ibm.com/customer-engagement/docs/watson-marketing/ibm-engage-2/watson-campaign-automation-platform/xml-api/codesamplesresponses/#CodeSample%E2%80%93PHP
+	{				
 		$this->load->library('watsonmarketing');
-		$pod = 8;
-		$username = 'username';
-		$password = 'password';
-
-		$endpoint = "https://api{$pod}.silverpop.com/XMLAPI";
-		$jsessionid = null;
-
-		$baseXml = '%s';
-		$loginXml = '<Envelope><Body>
-						<Login>
-						<USERNAME>%s</USERNAME>
-						<PASSWORD>%s</PASSWORD>
-						</Login>
-						</Body>
-					</Envelope>';
-		$getListsXml = '<Envelope><Body>
-							<GetLists>
-							<VISIBILITY>%d</VISIBILITY>
-							<LIST_TYPE>%d</LIST_TYPE>
-							</GetLists>
-							</Body>
-						</Envelope>';
-		$logoutXml = '<Envelope><Body>
-						<Logout/>
-						</Body>
-					</Envelope>';
-
 		try {
-
-			$xml = sprintf($baseXml, sprintf($loginXml, $username, $password));
-
-			$result = $this->watsonmarketing->xmlToArray($this->watsonmarketing->makeRequest($endpoint, $jsessionid, $xml));			
-			
-			if (!isset($result['SESSIONID']) || empty($result['SESSIONID'])) {
-				die("\n\n <br> Error: No se ha podido establecer session con los datos especificados\n\n");
-			}
-			$jsessionid = $result['SESSIONID'];
-		
-			$xml = sprintf($baseXml, sprintf($getListsXml, 1, 2)); // VISIBILITY 1 = Shared, LIST_TYPE 2 = Regular and Query
-			
-			$result = $this->watsonmarketing->xmlToArray($this->watsonmarketing->makeRequest($endpoint, $jsessionid, $xml));
-			print_r($result); echo "<br><br>";
-		
-			$xml = $logoutXml;
-			$result = $this->watsonmarketing->xmlToArray($this->watsonmarketing->makeRequest($endpoint, $jsessionid, $xml, true));
-			print_r($result);
-		
-			$jsessionid = null;
-		
-			print "\nDone\n\n";
+			$sesion = $this->watsonmarketing->login("usuario", "contraseña");
+			$lista = $this->watsonmarketing->getLists();
+			$cerrar =$this->watsonmarketing->logout();
+			print_r($lista);
 		} catch (Exception $e) {
-
 			die("\nException caught: {$e->getMessage()}\n\n");
 		}
-
+		
 	}
 	
 }
--- application/libraries/Watsonmarketing.php
+++ application/libraries/Watsonmarketing.php
@@ -2,9 +2,12 @@
 
 class Watsonmarketing {
 
-    function makeRequest($endpoint, $jsessionid, $xml, $ignoreResult = false)
+    private $endpoint = "https://api8.silverpop.com/XMLAPI";   
+    private $baseXml = '%s';
+
+    function makeRequest($jsessionid, $xml, $ignoreResult = false)
     {
-        $url = $this->getApiUrl($endpoint, $jsessionid);
+        $url = $this->getApiUrl($this->endpoint, $jsessionid);
         
         $xmlObj = new SimpleXmlElement($xml);
         $request = $xmlObj->asXml();
@@ -67,6 +70,64 @@ class Watsonmarketing {
     {
         $json = $this->xmlToJson($xml);
         return json_decode($json, true);
+    }
+
+    // functions for the XML API watson Marketing
+    function login($username, $password){
+        $loginXml = '<Envelope><Body>
+						<Login>
+						<USERNAME>%s</USERNAME>
+						<PASSWORD>%s</PASSWORD>
+						</Login>
+						</Body>
+                    </Envelope>';
+        
+        $xml = sprintf($this->baseXml, sprintf($loginXml, $username, $password));
+        $result = $this->xmlToArray($this->makeRequest(null, $xml));			
+        
+        if (!isset($result['SESSIONID']) || empty($result['SESSIONID'])) {
+            return array("tipo"=>"error", 
+                        "referencia"=>"Por favor verifique el usuario y la contraseña");
+        }else{            
+            $_SESSION['SESSIONID'] = $result['SESSIONID'];
+            return array("tipo"=>"success",
+                        "token"=>$_SESSION['SESSIONID']);
+        }
+        
+    }
+
+    function logout(){
+        $logoutXml = '<Envelope><Body>
+						<Logout/>
+						</Body>
+                    </Envelope>';
+        $_SESSION['SESSIONID'] = (isset($_SESSION['SESSIONID'])?$_SESSION['SESSIONID']:null);
+        $result = $this->xmlToArray($this->makeRequest($_SESSION['SESSIONID'], $logoutXml, true));
+        unset($_SESSION['SESSIONID']);
+        if ($result['RESULT']['SUCCESS']==TRUE) {            
+            return array("tipo"=>"success",
+                        "mensaje"=>"La session ha sido cerrada exitosamente.");
+        }else{
+            return array("tipo"=>"error",
+                        "mensaje"=>"La session no se ha podido cerrar.");
+        }
+    }
+
+    function getLists(){
+        if (!isset($_SESSION['SESSIONID'])) {
+            return json_encode(array("tipo"=>"error", "mensaje"=>"error en la session de usuario"));
+        }
+        $getListsXml = '<Envelope><Body>
+							<GetLists>
+							<VISIBILITY>%d</VISIBILITY>
+							<LIST_TYPE>%d</LIST_TYPE>
+							</GetLists>
+							</Body>
+                        </Envelope>';
+        $xml = sprintf($this->baseXml, sprintf($getListsXml, 1, 2)); // VISIBILITY 1 = Shared, LIST_TYPE 2 = Regular and Query
+
+        $result = $this->xmlToArray($this->makeRequest($_SESSION['SESSIONID'], $xml));
+        return json_encode($result);
     }
 
 }
